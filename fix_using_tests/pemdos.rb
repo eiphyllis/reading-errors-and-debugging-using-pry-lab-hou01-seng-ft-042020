18e4298973b0b28a5eb867e1d15bef862b3ed471
Phyllis N <phyllisgetcity@gmail.com>
2020-04-04 17:24:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
+puts "hello"
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+   cow =  10 * "s" + string
+   puts cow
+   binding.pry
   else
     string
   end
